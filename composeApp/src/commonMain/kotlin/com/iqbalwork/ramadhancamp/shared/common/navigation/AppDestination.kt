86ad36dad22ac22d95a0885f05a8f189e4d6f9bb
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2026-02-23 11:27:34 +0700

    feat(nav): add per-tab destinations and sheet destinations, remove SampleDialog

--- composeApp/src/commonMain/kotlin/com/iqbalwork/ramadhancamp/shared/common/navigation/AppDestination.kt
+++ composeApp/src/commonMain/kotlin/com/iqbalwork/ramadhancamp/shared/common/navigation/AppDestination.kt
@@ -3,74 +3,75 @@ package com.iqbalwork.ramadhancamp.shared.common.navigation
 import androidx.navigation3.runtime.NavKey
 import androidx.savedstate.serialization.SavedStateConfiguration
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 
 enum class AppTab { Home, Pray, Quran, Qibla, Bookmark }
 
-// Root Level
 @Serializable
 sealed interface RootDestination : NavKey {
-    @Serializable
-    data class Main(val initialTab: AppTab = AppTab.Home) : RootDestination
-
-    @Serializable
-    data object Auth : RootDestination
+    @Serializable data class Main(val initialTab: AppTab = AppTab.Home) : RootDestination
+    @Serializable data object Auth : RootDestination
 }
 
-// Nested Level
 @Serializable
 sealed interface TabDestination : NavKey {
-
-    // Home Level
-    @Serializable
-    data object HomeMain : TabDestination
-
-    // Pray Level
-    @Serializable
-    data object PrayMain : TabDestination
-
-    // Quran Level
-    @Serializable
-    data object QuranMain : TabDestination
-
-    // Qibla Level
-    @Serializable
-    data object QiblaMain : TabDestination
-
-    // Bookmark Level
-    @Serializable
-    data object BookmarkMain : TabDestination
-
-    // Home Detail
-    @Serializable
-    data object HomeDetail : TabDestination
+    // Home
+    @Serializable data object HomeMain       : TabDestination
+    @Serializable data object HomeDetail     : TabDestination
+    @Serializable data object HomeSubDetail  : TabDestination
+    // Pray
+    @Serializable data object PrayMain       : TabDestination
+    @Serializable data object PrayDetail     : TabDestination
+    @Serializable data object PraySubDetail  : TabDestination
+    // Quran
+    @Serializable data object QuranMain      : TabDestination
+    @Serializable data object QuranDetail    : TabDestination
+    @Serializable data object QuranSubDetail : TabDestination
+    // Qibla
+    @Serializable data object QiblaMain      : TabDestination
+    @Serializable data object QiblaDetail    : TabDestination
+    @Serializable data object QiblaSubDetail : TabDestination
+    // Bookmark
+    @Serializable data object BookmarkMain      : TabDestination
+    @Serializable data object BookmarkDetail    : TabDestination
+    @Serializable data object BookmarkSubDetail : TabDestination
 }
 
-// Dialog Navigation (bottom sheet, dialog, etc.)
 @Serializable
-sealed interface DialogDestination: NavKey {
-
-    @Serializable
-    data object SampleDialog : DialogDestination
-
+sealed interface DialogDestination : NavKey {
+    @Serializable data object HomeSheet     : DialogDestination
+    @Serializable data object PraySheet     : DialogDestination
+    @Serializable data object QuranSheet    : DialogDestination
+    @Serializable data object QiblaSheet    : DialogDestination
+    @Serializable data object BookmarkSheet : DialogDestination
 }
 
 val appSavedStateConfig = SavedStateConfiguration {
     serializersModule = SerializersModule {
         polymorphic(NavKey::class) {
-            subclass(RootDestination.Main::class, RootDestination.Main.serializer())
-            subclass(RootDestination.Auth::class, RootDestination.Auth.serializer())
-            subclass(TabDestination.HomeMain::class, TabDestination.HomeMain.serializer())
-            subclass(TabDestination.PrayMain::class, TabDestination.PrayMain.serializer())
-            subclass(TabDestination.QuranMain::class, TabDestination.QuranMain.serializer())
-            subclass(TabDestination.QiblaMain::class, TabDestination.QiblaMain.serializer())
-            subclass(TabDestination.BookmarkMain::class, TabDestination.BookmarkMain.serializer())
-            subclass(TabDestination.HomeDetail::class, TabDestination.HomeDetail.serializer())
-            subclass(DialogDestination.SampleDialog::class, DialogDestination.SampleDialog.serializer())
+            subclass(RootDestination.Main::class,          RootDestination.Main.serializer())
+            subclass(RootDestination.Auth::class,          RootDestination.Auth.serializer())
+            subclass(TabDestination.HomeMain::class,          TabDestination.HomeMain.serializer())
+            subclass(TabDestination.HomeDetail::class,        TabDestination.HomeDetail.serializer())
+            subclass(TabDestination.HomeSubDetail::class,     TabDestination.HomeSubDetail.serializer())
+            subclass(TabDestination.PrayMain::class,          TabDestination.PrayMain.serializer())
+            subclass(TabDestination.PrayDetail::class,        TabDestination.PrayDetail.serializer())
+            subclass(TabDestination.PraySubDetail::class,     TabDestination.PraySubDetail.serializer())
+            subclass(TabDestination.QuranMain::class,         TabDestination.QuranMain.serializer())
+            subclass(TabDestination.QuranDetail::class,       TabDestination.QuranDetail.serializer())
+            subclass(TabDestination.QuranSubDetail::class,    TabDestination.QuranSubDetail.serializer())
+            subclass(TabDestination.QiblaMain::class,         TabDestination.QiblaMain.serializer())
+            subclass(TabDestination.QiblaDetail::class,       TabDestination.QiblaDetail.serializer())
+            subclass(TabDestination.QiblaSubDetail::class,    TabDestination.QiblaSubDetail.serializer())
+            subclass(TabDestination.BookmarkMain::class,      TabDestination.BookmarkMain.serializer())
+            subclass(TabDestination.BookmarkDetail::class,    TabDestination.BookmarkDetail.serializer())
+            subclass(TabDestination.BookmarkSubDetail::class, TabDestination.BookmarkSubDetail.serializer())
+            subclass(DialogDestination.HomeSheet::class,      DialogDestination.HomeSheet.serializer())
+            subclass(DialogDestination.PraySheet::class,      DialogDestination.PraySheet.serializer())
+            subclass(DialogDestination.QuranSheet::class,     DialogDestination.QuranSheet.serializer())
+            subclass(DialogDestination.QiblaSheet::class,     DialogDestination.QiblaSheet.serializer())
+            subclass(DialogDestination.BookmarkSheet::class,  DialogDestination.BookmarkSheet.serializer())
         }
     }
 }
-
-
